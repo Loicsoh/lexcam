1b089b7376882c4003b5a5d33386eea51c3032c2
Loicsoh <www.sohloic132@gmail.com>
2025-02-27 02:14:29 +0100

    style formulaire

--- src/Penal.jsx
+++ src/Penal.jsx
@@ -4,9 +4,9 @@ const Penal = () => {
         <h1><img src="/src/assets/images/img03.jpg" alt="" />Connaitre le code Penal du cameroun<img src="/src/assets/images/img03.jpg" alt=""/></h1>
 
             <div className="form">
-                <h2>Entrez numéro d'article alineat du code Pénal</h2>
+                <h2>Entrez numéro d&apos;article alineat du code Pénal</h2>
                 <form>
-                    <label htmlFor="alineatNumber">Numéro d'alinéa:</label>
+                    <label htmlFor="alineatNumber">Numéro d\'alinéa:</label>
                     <input type="text" id="alineatNumber" name="alineatNumber" />
                     <button type="submit">Rechercher</button>
                 </form>
