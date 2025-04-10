565ecf6f2d9dc356e9eeae7fa0dca8676d4567b1
Loicsoh <www.sohloic132@gmail.com>
2025-04-10 11:21:48 +0200

    hio

--- src/Livrecodecivil.jsx
+++ src/Livrecodecivil.jsx
@@ -32,6 +32,49 @@ const Livrecodecivil = () => {
     };
 
     // fonction de rendu des articles
+
+    const randerArticle = (articles) => {
+        return (
+            <ul className="article-list">
+                {article.map((article.article) => (
+                    <li key={article.articles} className="article-item">
+                        <strong>Article{article.article}:{article.explication}</strong>
+                    </li>
+                ))}
+            </ul>
+        )
+    };
+
+    // fonction de rendu des chapitres
+
+    const randerchapter = (chapitres) => {
+        return(
+            <div>
+                {chapitres.map((chapitre) => (
+                    <div key={chapitre.chapitre} className="chapitre">
+                        <h3 className="article-item">{chapitre.name}</h3>
+                        {randerArticle(chapitre.articles)
+                        ? randerArticle(chapitre.articles)
+                        : null}
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    // fonction de rendu des livres
+    const randerLivre = (livres) => {
+        return(
+            <div>
+                {livres.map((livre) => (
+                    <div key={livre.livre} className="livre">
+                        <h2 className="article-item">{livre.name}</h2>
+                        {randerchapter(livre.chapitres)}
+                    </div>
+                ))}
+            </div>
+        )
+    }
     
     return ( 
         <h1>liste des livres du code civil du cameroun</h1>
